af05669b467784294e4842f670ebf8c9564c745b
JosueFS <josue.18rj@gmail.com>
2020-12-03 19:42:21 -0300

    Fixed the ContextApi usage :hammer:

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,19 @@ import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/globalStyle';
 
-import { AuthProvider } from './hooks/auth';
+import AppProvider from './hooks';
 
+import Header from './components/Header';
 import Footer from './components/Footer';
 import Routes from './routes';
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <AuthProvider>
+    <AppProvider>
+      <Header />
       <Routes />
-    </AuthProvider>
-
-    <Footer />
+      <Footer />
+    </AppProvider>
     <GlobalStyle />
   </BrowserRouter>
 );
