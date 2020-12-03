ffdb2e3f7c426e4e838c84a846312ff899e1a3df
JosueFS <josue.18rj@gmail.com>
2020-12-03 20:08:34 -0300

    Refactor: api consumed and Header decoupling

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { MdExitToApp } from 'react-icons/md';
+
 import ReactMarkdown from 'react-markdown';
 
 import {
   Container,
   Profile,
-  Header,
-  Logout,
   Avatar,
   UserInfo,
   ProfileInfo,
@@ -18,7 +16,7 @@ import api from '../../services/api';
 import { useAuth } from '../../hooks/auth';
 
 const User: React.FC = () => {
-  const { signOut, user } = useAuth();
+  const { user } = useAuth();
 
   const [readme, setReadme] = useState('');
 
@@ -34,51 +32,35 @@ const User: React.FC = () => {
 
   return (
     <Container>
-      <Header>
-        <strong>#josuefs</strong>
-
-        <Logout onClick={signOut}>
-          Sair
-          <MdExitToApp size={24} color="#D03434" />
-        </Logout>
-      </Header>
       <Profile>
-        <Avatar
-          alt="user"
-          src="https://avatars0.githubusercontent.com/u/45906601?s=460&v=4"
-        />
+        <Avatar alt="user" src={user.avatar_url} />
 
         <UserInfo>
-          <h2>JOSUÉ FERREIRA</h2>
-          <span>josue.18rj@gmail.com</span>
-          <span>Belford Roxo/RJ</span>
+          <h2>{user.name.toUpperCase()}</h2>
+          <span>{user.email}</span>
+          <span>{user.location}</span>
         </UserInfo>
 
         <ProfileInfo>
           <div>
-            <h1>70</h1>
+            <h1>{user.followers}</h1>
             <span>Seguidores</span>
           </div>
 
           <div>
-            <h1>77</h1>
+            <h1>{user.following}</h1>
             <span>Seguindo</span>
           </div>
 
           <div>
-            <h1>707</h1>
+            <h1>{user.public_repos}</h1>
             <span>Repos</span>
           </div>
         </ProfileInfo>
 
         <Bio>
           <h2>BIO</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet modi
-            illo pariatur porro, veritatis, accusantium ea quaerat minus a enim
-            commodi, sit aliquam! Harum a voluptatibus dolorem inventore
-            accusantium corporis?
-          </p>
+          <p>{user.bio}</p>
         </Bio>
       </Profile>
       <ReadMe>
